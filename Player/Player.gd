5cf1e6374ed2308fd7ef93dfe7ad8368772a619f
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2018-06-30 12:18:45 +0200

    Animation Playing

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,11 +4,21 @@ extends RigidBody2D
 # var a = 2
 # var b = "textvar"
 
+enum MovementState {
+	STANDING,
+	WALKING,
+	JUMPING,
+	FALLING
+}
+
 export (int) var playerId = 1
 export (int) var movementVelocity = 100
 export (int) var jumpVelocity = 200
 
 var upDirection
+var inputMovementDirection
+var movementState = MovementState.STANDING
+var currentLinearVelocity
 
 const FLOOR_COLLISION_AVOIDANCE_DISTANCE = 0.1
 
@@ -21,10 +31,36 @@ func _ready():
 		upDirection = Vector2(0, 1)
 	
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func currentMovementState():
+	if onFloor():
+		if inputMovementDirection == Vector2(0, 0):
+			return MovementState.STANDING
+		else:
+			return MovementState.WALKING
+	elif currentLinearVelocity.y * upDirection.y > 0:
+		return MovementState.JUMPING
+	else:
+		return MovementState.FALLING
+
+func _process(delta):
+	if playerId != 1:
+		return
+	var nextMovementState = currentMovementState()
+	if movementState != nextMovementState:
+		movementState = nextMovementState
+		var animationName
+		match movementState:
+			MovementState.STANDING:
+				animationName = "standing"
+			MovementState.WALKING:
+				animationName = "walking"
+			MovementState.JUMPING:
+				animationName = "jumping"
+			MovementState.FALLING:
+				animationName = "falling"
+		$AnimationPlayer.play(animationName)
+			
+
 
 func movementDirectionFromInput():
 	var direction = Vector2(0, 0)
@@ -44,9 +80,11 @@ func _integrate_forces(state):
 	var velocity = Vector2(0, 0)
 	if (requestsJump() && onFloor()):
 		velocity += upDirection * jumpVelocity
-	velocity += movementDirectionFromInput() * movementVelocity
+	inputMovementDirection = movementDirectionFromInput()
+	velocity += inputMovementDirection * movementVelocity
 	state.linear_velocity += velocity
 	state.linear_velocity.x = clamp(state.linear_velocity.x, -movementVelocity, movementVelocity)
+	currentLinearVelocity = state.linear_velocity
 	if (onFloor()):
 		state.transform.origin += upDirection * FLOOR_COLLISION_AVOIDANCE_DISTANCE
 	
