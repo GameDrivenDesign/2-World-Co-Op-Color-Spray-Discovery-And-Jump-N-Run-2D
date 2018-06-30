8c8ad93b1cfa0592ebbc3206302bc5a22272b0f9
Christian Flach <cmfcmf.flach@gmail.com>
2018-06-30 21:44:21 +0200

    Fix vertical camera movement

--- Levels/Camera.gd
+++ Levels/Camera.gd
@@ -15,23 +15,24 @@ func _ready():
 	player2 = get_node(player2_path)
 
 func _process(delta):
-	var p1 = player1.global_position
-	var p2 = player2.global_position
 	var screen_size = get_viewport().size
+	var half_screen_height = screen_size.y / 2
 	
-	var middle = (p1 + p2) / 2
-	var distance = (p1 - p2).abs()
+	var p1_pos = player1.global_position
+	var p2_pos = player2.global_position
+	var p1_screen_center_dist = abs(p1_pos.y - global_position.y)
+	var p2_screen_center_dist = abs(p2_pos.y - global_position.y)
+	
+	var middle_x = (p1_pos.x + p2_pos.x) / 2
+	var distance_x = abs(p1_pos.x - p2_pos.x)
 	
 	# Move the camera left and right, but never move it vertically.
-	self.global_position.x = middle.x
+	global_position.x = middle_x
 	
 	# Zoom the camera. Bigger zoom factors leads to zooming out.
 	# The lower bound zoom is set to MIN_ZOOM_FACTOR, so that we never zoom nearer than initially. 
-	var zoom_x = distance.x / screen_size.x * BEFORE_REACHING_SIDE_ZOOM_FACTOR
-	var zoom_y = max(
-		screen_size.y / 2 - player1.get_global_transform_with_canvas().origin.y,
-		player2.get_global_transform_with_canvas().origin.y - screen_size.y / 2
-	) / (screen_size.y / 2)
+	var zoom_x = distance_x / screen_size.x * BEFORE_REACHING_SIDE_ZOOM_FACTOR
+	var zoom_y = max(p1_screen_center_dist, p2_screen_center_dist) / half_screen_height * BEFORE_REACHING_SIDE_ZOOM_FACTOR
 	var zoom_factor = min(max(max(zoom_x, zoom_y), MIN_ZOOM_FACTOR), MAX_ZOOM_FACTOR)
-	$Camera.zoom = Vector2(zoom_factor, zoom_factor)
 	
+	$Camera.zoom = Vector2(zoom_factor, zoom_factor)
