69223757bc50b0e984a46b82705802b590c359b5
Alexander Riese <alexander.riese@student.hpi.de>
2018-06-30 13:11:57 +0200

    add color to tileset mapping

--- Player/Player.gd
+++ Player/Player.gd
@@ -48,7 +48,7 @@ func disposeColor():
 		var tilePoint = playerPos + Vector2(0, -upDirection.y * playerExt.y -upDirection.y)
 		var tilePos = map.world_to_map(tilePoint)
 		var currentTileIndex = map.get_cellv(tilePos)
-		map.set_cellv(tilePos, 1)
+		map.set_cellv(tilePos, Colors.color_name_to_tile_index("blue"))
 
 func _integrate_forces(state):
 	var velocity = Vector2(0, 0)
